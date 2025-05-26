669e90f6d7ca089f11371bd983b6f060f5d3fb59
crazyfrank <axu9417@gmail.com>
2025-05-26 09:14:10 +0800

    feat(autorestartpod):declare crd to controller autorestart pod

---
+++ api/v1/autorestartpod_types.go
@@ -0,0 +1,61 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// AutoRestartPodSpec defines the desired state of AutoRestartPod.
+type AutoRestartPodSpec struct {
+	Schedule string               `json:"schedule"`           // Define Cron expressions (e.g. "0 3 * * * *")
+	Selector metav1.LabelSelector `json:"selector"`           // Define the label selector for the Pod to be restarted.
+	TimeZone string               `json:"timeZone,omitempty"` // Optional: time zone (e.g. "Asia/Shanghai")
+}
+
+// AutoRestartPodStatus defines the observed state of AutoRestartPod.
+type AutoRestartPodStatus struct {
+	LastRestartTime *metav1.Time `json:"lastRestartTime,omitempty"` // Record the last reboot time
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// AutoRestartPod is the Schema for the autorestartpods API.
+type AutoRestartPod struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   AutoRestartPodSpec   `json:"spec,omitempty"`
+	Status AutoRestartPodStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// AutoRestartPodList contains a list of AutoRestartPod.
+type AutoRestartPodList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []AutoRestartPod `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&AutoRestartPod{}, &AutoRestartPodList{})
+}
---
+++ internal/controller/autorestartpod_controller.go
@@ -0,0 +1,99 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	"time"
+
+	"github.com/robfig/cron/v3"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+
+	stablev1 "github.com/crazyfrankie/autorestart-operator/api/v1"
+)
+
+// AutoRestartPodReconciler reconciles a AutoRestartPod object
+type AutoRestartPodReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=stable.crazyfrank.com,resources=autorestartpods,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=stable.crazyfrank.com,resources=autorestartpods/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=stable.crazyfrank.com,resources=autorestartpods/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state,
+// the AutoRestartPod object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.21.0/pkg/reconcile
+func (r *AutoRestartPodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := logf.FromContext(ctx)
+
+	obj := &stablev1.AutoRestartPod{}
+	if err := r.Get(ctx, req.NamespacedName, obj); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	schedule, err := cron.ParseStandard(obj.Spec.Schedule)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	nextRun := schedule.Next(time.Now())
+	if now := time.Now(); nextRun.After(now) {
+		return ctrl.Result{RequeueAfter: nextRun.Sub(now)}, nil
+	}
+
+	podList := &corev1.PodList{}
+	selector, _ := metav1.LabelSelectorAsSelector(&obj.Spec.Selector)
+	if err = r.List(ctx, podList, client.InNamespace(req.Namespace),
+		client.MatchingLabelsSelector{Selector: selector}); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	for _, pod := range podList.Items {
+		if err := r.Delete(ctx, &pod); err != nil {
+			log.Error(err, "Failed to delete pod", "pod ", pod.Name)
+		} else {
+			log.Info("Restart pod", "pod ", pod.Name)
+		}
+	}
+
+	obj.Status.LastRestartTime = &metav1.Time{Time: time.Now()}
+	if err := r.Status().Update(ctx, obj); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	return ctrl.Result{RequeueAfter: schedule.Next(time.Now()).Sub(time.Now())}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *AutoRestartPodReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&stablev1.AutoRestartPod{}).
+		Named("autorestartpod").
+		Complete(r)
+}
